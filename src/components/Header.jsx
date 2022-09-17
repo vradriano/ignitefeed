63ca2af96d9333cef62677aa2496164f6851bb47
unknown <vradriano@hotmail.com>
2022-09-16 21:58:50 -0300

    Header component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,11 @@
 import styles from './Header.module.css'
 
+import igniteLogo from '../assets/Ignite-logo.svg'
+
 export function Header() {
   return (
     <header className={styles.header}>
-      <strong>Ignite Feed</strong>
+      <img src={igniteLogo} alt="Ignite Logo" />
     </header>
   )
 }
